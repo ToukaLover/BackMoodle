6ea77e64b568bffc5f641f360ceec3af1c9330a9
ToukaLover <antoniodeurda@gmail.com>
2025-05-07 13:20:32 +0200

    EscucharConexiones

--- src/chat-ws/chat-ws.gateway.ts
+++ src/chat-ws/chat-ws.gateway.ts
@@ -1,13 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { ChatWsService } from './chat-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({cors:true})
-export class ChatWsGateway {
+export class ChatWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
   constructor(private readonly chatWsService: ChatWsService) {}
 
-  
+  handleDisconnect(client: Socket) {
+    console.log("Cliente desconectado",client.id)
+  }
 
-
-
+  handleConnection(client: Socket) {
+    console.log("Cliente conectado",client.id)
+  }
   
 }
