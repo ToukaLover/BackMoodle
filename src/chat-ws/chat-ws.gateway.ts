87940baebf3ae7f2da85bd817a17a3f54fc9a01f
ToukaLover <antoniodeurda@gmail.com>
2025-05-08 08:44:04 +0200

    EscuchaServerYVerificarToken

--- src/chat-ws/chat-ws.gateway.ts
+++ src/chat-ws/chat-ws.gateway.ts
@@ -1,20 +1,42 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ChatWsService } from './chat-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({ cors: true })
 export class ChatWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly chatWsService: ChatWsService) { }
+  
+  //Server para poder mandar cosas a los demas clientes / Siempre es asi y es de socket.io
+  @WebSocketServer() wss : Server
+  
+  constructor(private readonly chatWsService: ChatWsService,
+      private readonly authService : AuthService
+  ) { }
 
 
-  handleConnection(client: Socket) {
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.token as string
+
     this.chatWsService.registerClient(client)
-    console.log({ conectados: this.chatWsService.getConnectedClients() })
 
+    const tokenData = await this.authService.verify(token)
+
+    console.log(tokenData)
   }
 
   handleDisconnect(client: Socket) {
     this.chatWsService.removeClient(client.id)
   }
+
+  //SuscribeMessage escucha mensajes
+  // on escucha, emit envia, to es a varios o a quien quieras por Id emit/on ('nombre-evento', (funcion/Que envia))
+  @SubscribeMessage('message-from-user') //El cliente me lo da el propio SubscribeMessage
+  handleMessageFromUser(client : Socket, payload : any){
+
+
+
+  }
+
 
 }
--- src/chat-ws/chat-ws.module.ts
+++ src/chat-ws/chat-ws.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChatWsService } from './chat-ws.service';
 import { ChatWsGateway } from './chat-ws.gateway';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
-  providers: [ChatWsGateway, ChatWsService],
+  providers: [ChatWsGateway, ChatWsService,AuthService],
 })
 export class ChatWsModule {}
